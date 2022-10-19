ecd31068e337c86a3c252736383208c445809796
Fumiyoshi MATANO <f_matano44@yahoo.co.jp>
2022-10-19 16:40:13 +0900

    依存外部ソフトのライセンスを追加

--- tools/segmentation-kit/segment_julius.pl
+++ tools/segmentation-kit/segment_julius.pl
@@ -46,7 +46,7 @@ $leave_dict_flag=0;
 $debug_flag=0;
 
 ## julius executable
-$juliusbin="./bin/julius-4.6";
+$juliusbin="./bin/julius/julius-4.6";
 
 ## acoustic model
 $hmmdefs="./models/hmmdefs_monof_mix16_gid.binhmm"; # monophone model
